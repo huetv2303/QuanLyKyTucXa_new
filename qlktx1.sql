d989fbe6717d35b971c857530e354e9854338ea5
DTHV19 <168798056+DTHV19@users.noreply.github.com>
2024-06-21 11:22:29 +0700

    Add files via upload
    file database moi cua van

--- qlktx1.sql
+++ qlktx1.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 20, 2024 at 10:58 AM
+-- Generation Time: Jun 21, 2024 at 06:19 AM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `qlktx1`
+-- Database: `qlktx`
 --
 
 -- --------------------------------------------------------
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `dich_vu_dien_nuoc` (
   `id_service` varchar(50) NOT NULL,
   `name_service` varchar(100) NOT NULL,
-  `price` decimal(10,2) NOT NULL,
+  `price` double NOT NULL,
   `unit` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -39,8 +39,8 @@ CREATE TABLE `dich_vu_dien_nuoc` (
 --
 
 INSERT INTO `dich_vu_dien_nuoc` (`id_service`, `name_service`, `price`, `unit`) VALUES
-('Dien', 'Dịch vụ điện', 4000.00, 'kWh'),
-('Nuoc', 'Dịch vụ nước', 15000.00, 'm3');
+('Dien', 'Dịch vụ điện', 4000, 'kWh'),
+('Nuoc', 'Dịch vụ nước', 15000, 'm3');
 
 -- --------------------------------------------------------
 
@@ -51,21 +51,18 @@ INSERT INTO `dich_vu_dien_nuoc` (`id_service`, `name_service`, `price`, `unit`) 
 CREATE TABLE `dich_vu_khac` (
   `id_service` varchar(50) NOT NULL,
   `name_service` varchar(100) NOT NULL,
-  `price` decimal(10,2) NOT NULL,
-  `unit` varchar(50) NOT NULL,
-  `note` varchar(255) NOT NULL
+  `price` double NOT NULL,
+  `unit` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `dich_vu_khac`
 --
 
-INSERT INTO `dich_vu_khac` (`id_service`, `name_service`, `price`, `unit`, `note`) VALUES
-('DV01', 'internet', 300000.00, 'Tháng', ''),
-('DV02', 'Rác', 40000.00, 'Tháng', ''),
-('DV03', 'Giặt là', 20000.00, 'Tháng', ''),
-('DV05', 'Xe', 300000.00, 'Tháng', ''),
-('DV08', 'TV', 1000000.00, 'Tháng', '');
+INSERT INTO `dich_vu_khac` (`id_service`, `name_service`, `price`, `unit`) VALUES
+('DV01', 'Internet', 300000, 'tháng'),
+('DV02', 'Dịch vụ giặt là', 50000, 'lần'),
+('DV05', 'Xe', 300000, 'Tháng');
 
 -- --------------------------------------------------------
 
@@ -76,10 +73,8 @@ INSERT INTO `dich_vu_khac` (`id_service`, `name_service`, `price`, `unit`, `note
 CREATE TABLE `hoa_don_dich_vu` (
   `id_invoice` varchar(50) NOT NULL,
   `id_room` varchar(50) NOT NULL,
-  `electricity` decimal(10,0) NOT NULL,
-  `water` decimal(10,0) NOT NULL,
-  `created_day` date NOT NULL,
-  `ended_day` date DEFAULT NULL,
+  `electricity` double NOT NULL,
+  `water` double NOT NULL,
   `status` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -87,11 +82,54 @@ CREATE TABLE `hoa_don_dich_vu` (
 -- Dumping data for table `hoa_don_dich_vu`
 --
 
-INSERT INTO `hoa_don_dich_vu` (`id_invoice`, `id_room`, `electricity`, `water`, `created_day`, `ended_day`, `status`) VALUES
-('HD003', 'P003', 5, 6, '2024-06-19', '2024-06-20', 'Chưa thanh toán'),
-('HD004', 'P004', 5, 5, '2024-06-15', '2024-06-20', 'Chưa thanh toán'),
-('HD01', 'P002', 5, 5, '0000-00-00', '0000-00-00', 'Chưa thanh toán'),
-('HD10', 'P001', 30, 4, '2024-07-23', '2024-07-30', 'Chưa thanh toán');
+INSERT INTO `hoa_don_dich_vu` (`id_invoice`, `id_room`, `electricity`, `water`, `status`) VALUES
+('HD003', 'P002', 80, 4, 'Đã thanh toán');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hopdong`
+--
+
+CREATE TABLE `hopdong` (
+  `maHopDong` varchar(50) NOT NULL,
+  `maNhanVien` varchar(50) NOT NULL,
+  `maSinhVien` varchar(50) NOT NULL,
+  `maPhong` varchar(50) NOT NULL,
+  `ngayBatDau` date NOT NULL,
+  `ngayKetThuc` date NOT NULL,
+  `tinhTrang` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nhanvien`
+--
+
+CREATE TABLE `nhanvien` (
+  `maNhanVien` varchar(10) NOT NULL,
+  `tenNhanVien` varchar(50) NOT NULL,
+  `gioiTinh` varchar(10) NOT NULL,
+  `ngaySinh` date NOT NULL,
+  `diaChi` varchar(100) NOT NULL,
+  `soDienThoai` varchar(20) NOT NULL,
+  `maToa` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `phong`
+--
+
+CREATE TABLE `phong` (
+  `maPhong` varchar(50) NOT NULL,
+  `maToa` varchar(50) NOT NULL,
+  `soNguoi` int(50) NOT NULL,
+  `tienPhong` int(50) NOT NULL,
+  `trangThai` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -110,34 +148,48 @@ CREATE TABLE `phong_dich_vu` (
 --
 
 INSERT INTO `phong_dich_vu` (`id`, `id_room`, `id_service`) VALUES
-(24, 'P003', 'DV03'),
-(25, 'P003', 'DV02'),
-(28, 'P001', 'DV01'),
-(29, 'P001', 'DV05'),
-(30, 'P002', 'DV01'),
-(32, 'P004', 'DV01'),
-(33, 'P004', 'DV01'),
-(34, 'P004', 'DV01');
+(1, 'P001', 'DV01'),
+(2, 'P001', 'DV02'),
+(3, 'P002', 'DV01'),
+(4, 'P002', 'DV02'),
+(5, 'P002', 'DV05');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `phong_ky_tuc_xa`
+-- Table structure for table `thongtinsinhvien`
 --
 
-CREATE TABLE `phong_ky_tuc_xa` (
-  `id_room` varchar(50) NOT NULL
+CREATE TABLE `thongtinsinhvien` (
+  `maSinhVien` varchar(50) NOT NULL,
+  `hoTen` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `soDienThoai` int(50) NOT NULL,
+  `ngaySinh` date NOT NULL,
+  `gioiTinh` varchar(50) NOT NULL,
+  `cccd` varchar(50) NOT NULL,
+  `diaChi` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `toa`
+--
+
+CREATE TABLE `toa` (
+  `maToa` varchar(50) NOT NULL,
+  `soNhanVien` int(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `phong_ky_tuc_xa`
+-- Dumping data for table `toa`
 --
 
-INSERT INTO `phong_ky_tuc_xa` (`id_room`) VALUES
-('P001'),
-('P002'),
-('P003'),
-('P004');
+INSERT INTO `toa` (`maToa`, `soNhanVien`) VALUES
+('MT1', 2),
+('MT2', 2),
+('MT3', 3);
 
 --
 -- Indexes for dumped tables
@@ -159,8 +211,25 @@ ALTER TABLE `dich_vu_khac`
 -- Indexes for table `hoa_don_dich_vu`
 --
 ALTER TABLE `hoa_don_dich_vu`
-  ADD PRIMARY KEY (`id_invoice`),
-  ADD KEY `id_room` (`id_room`);
+  ADD PRIMARY KEY (`id_invoice`);
+
+--
+-- Indexes for table `hopdong`
+--
+ALTER TABLE `hopdong`
+  ADD PRIMARY KEY (`maHopDong`);
+
+--
+-- Indexes for table `nhanvien`
+--
+ALTER TABLE `nhanvien`
+  ADD PRIMARY KEY (`maNhanVien`);
+
+--
+-- Indexes for table `phong`
+--
+ALTER TABLE `phong`
+  ADD PRIMARY KEY (`maPhong`);
 
 --
 -- Indexes for table `phong_dich_vu`
@@ -171,10 +240,16 @@ ALTER TABLE `phong_dich_vu`
   ADD KEY `id_service` (`id_service`);
 
 --
--- Indexes for table `phong_ky_tuc_xa`
+-- Indexes for table `thongtinsinhvien`
 --
-ALTER TABLE `phong_ky_tuc_xa`
-  ADD PRIMARY KEY (`id_room`);
+ALTER TABLE `thongtinsinhvien`
+  ADD PRIMARY KEY (`maSinhVien`);
+
+--
+-- Indexes for table `toa`
+--
+ALTER TABLE `toa`
+  ADD PRIMARY KEY (`maToa`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -184,24 +259,7 @@ ALTER TABLE `phong_ky_tuc_xa`
 -- AUTO_INCREMENT for table `phong_dich_vu`
 --
 ALTER TABLE `phong_dich_vu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `hoa_don_dich_vu`
---
-ALTER TABLE `hoa_don_dich_vu`
-  ADD CONSTRAINT `hoa_don_dich_vu_ibfk_1` FOREIGN KEY (`id_room`) REFERENCES `phong_ky_tuc_xa` (`id_room`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `phong_dich_vu`
---
-ALTER TABLE `phong_dich_vu`
-  ADD CONSTRAINT `phong_dich_vu_ibfk_1` FOREIGN KEY (`id_room`) REFERENCES `phong_ky_tuc_xa` (`id_room`),
-  ADD CONSTRAINT `phong_dich_vu_ibfk_2` FOREIGN KEY (`id_service`) REFERENCES `dich_vu_khac` (`id_service`);
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
